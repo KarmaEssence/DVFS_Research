56c24141b560870aa4faf79add8e401fb2ed66e6
Youssef-AEM <youssef_g10@hotmail.com>
2023-02-24 16:12:17 +0100

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -14,7 +14,7 @@ def main():
     mconst.create_folder_if_he_doesnt_exist(mconst.resource)
     # cu.launch() # Test 1 : Curves
     # si.launch() # Test 2 : Single
-    # la.launch() # Test 3 : Latex
+    # la.launch() # Test 3 : Latex : this test can take a bit longer because it compares all the methods for all the proposed instances.
     print("All test done !")
 
 
